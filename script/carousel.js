18318e5cb557df504e1a5e31efada342ab2b2fbe
Radevskii <37086021+Radevskii@users.noreply.github.com>
2020-06-14 21:36:36 +0300

    ["Update"] Last commit

--- script/carousel.js
+++ script/carousel.js
@@ -3,13 +3,12 @@ showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("my-slides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000);
 }
